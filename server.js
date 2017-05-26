b50c51418003b0d8b536006a65a0480b0bd28486
Robbie D'Antonio <robbie.dantonio@gmail.com>
2017-05-26 13:22:43 -0400

    trial and error 1

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io')(server);
 
+
 /*
 app.get('/', function(req, res,next){
 	res.sendFile(__dirname + '/public/index.html');
 });
 */
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-server.listen(app.get('port') || 3000, function(){
+/*app.use(express.static(__dirname + '/public'));*/
+
+server.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
 
